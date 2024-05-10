b79ca59423ae5f8fd6e788db7d6dab20c76c03db
Aureo-Bueno <aureoalexandre@outlook.com>
2024-05-09 21:27:43 -0300

    bugs: add other bugs to gabi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,19 @@ import { Logger, Module } from '@nestjs/common';
 import { CustomerModule } from './customer/customer.module';
 import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
-import { DataSource } from 'typeorm';
 import { TypeOrmConfigSQL } from './typeOrm.migration-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({useFactory: TypeOrmConfigSQL}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: TypeOrmConfigSQL,
+      inject: [ConfigModule],
+    }),
     CustomerModule,
-    ProductModule
+    ProductModule,
   ],
   controllers: [],
   providers: [Logger],
--- src/typeOrm.migration-config.ts
+++ src/typeOrm.migration-config.ts
@@ -1,14 +1,19 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const TypeOrmConfigSQL = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+export const TypeOrmConfigSQL = async (
+  configService: ConfigService
+): Promise<TypeOrmModuleOptions> => ({
   type: 'postgres',
-  host:configService.get('POSTGRES_HOST'),
+  host: configService.get('POSTGRES_HOST'),
   port: configService.get<number>('POSTGRES_PORT', 5432),
   username: configService.get('POSTGRES_USER'),
   password: configService.get('POSTGRES_PASSWORD'),
   database: configService.get('POSTGRES_DATABASE'),
   entities: [__dirname + '../**/*.entity.ts'],
   synchronize: true,
-  autoLoadEntities: true
+  autoLoadEntities: true,
+  extra: {
+    socketPath: configService.get('SOCKET_PATH'),
+  },
 });
