c6bc7ceaa6427dad294eb40fa8452bbb5aebb753
Aureo-Bueno <aureoalexandre@outlook.com>
2024-05-09 22:08:52 -0300

    fix: bugs connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { Logger, Module } from '@nestjs/common';
-import { CustomerModule } from './customer/customer.module';
-import { ProductModule } from './product/product.module';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmConfigSQL } from './typeOrm.migration-config';
+import { CustomerModule } from './modules/customer/customer.module';
+import { ProductModule } from './modules/product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmConfigSQL } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: TypeOrmConfigSQL,
-      inject: [ConfigModule],
+      inject: [ConfigService],
     }),
     CustomerModule,
     ProductModule,
