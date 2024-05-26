a4a6e29a229e7dbb82a55ed636178996121673fc
Aureo-Bueno <aureoalexandre@outlook.com>
2024-05-26 17:10:19 -0300

    feat: add types

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app: INestApplication<any> = await NestFactory.create(AppModule, {
     logger: ["warn", "debug", "error", "log"],
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  const config = new DocumentBuilder()
+  const config: Omit<OpenAPIObject, "paths"> = new DocumentBuilder()
     .setTitle('Tech Challenge Monolith API')
     .setDescription('API.')
     .setVersion('1.0')
     .build();
 
-  const configService = app.get<ConfigService>(ConfigService);
+  const configService: ConfigService<Record<string, unknown>, false> = app.get<ConfigService>(ConfigService);
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
   await app.listen(configService.get<number>('API_PORT'));
 }
+
 bootstrap();
